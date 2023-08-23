aa4dea692315944ffdbcc63083383fbcc9f8a29f
milovangudelj <milovan.gudelj@gmail.com>
2023-08-23 23:22:53 +0200

    Changes in styling

--- components/modules/Hero/Hero.tsx
+++ components/modules/Hero/Hero.tsx
@@ -10,27 +10,29 @@ export const HeroModule = ({
   photo,
 }: _HeroModule) => {
   return (
-    <div className="relative overflow-hidden px-8">
-      <Image
-        className="absolute inset-0 h-full w-full object-cover object-center"
-        src={urlForImage(photo).quality(100).url()}
-        placeholder="blur"
-        blurDataURL={photo.asset.metadata.lqip}
-        width={photo.asset.metadata.dimensions.width}
-        height={photo.asset.metadata.dimensions.height}
-        quality={100}
-        alt={photo.alt}
-      />
-      <div className="relative mx-auto flex max-w-7xl flex-col gap-8 py-32">
-        <h1 className="fv-width-150 text-h1-mobile text-white md:text-h1">
-          {title}
-        </h1>
-        <p>{content}</p>
-        {withButton && (
-          <button className="w-fit rounded-lg bg-orange-500 px-4 py-2 text-button text-white">
-            {buttonText}
-          </button>
-        )}
+    <div className="overflow-hidden px-8">
+      <div className="mx-auto flex max-w-7xl justify-between py-32">
+        <div className="flex flex-col gap-8">
+          <h1 className="fv-width-150 text-h1-mobile text-white md:text-h1">
+            {title}
+          </h1>
+          <p>{content}</p>
+          {withButton && (
+            <button className="w-fit rounded-lg bg-orange-500 px-4 py-2 text-button text-white">
+              {buttonText}
+            </button>
+          )}
+        </div>
+        <Image
+          className="block h-[300px] w-[400px] object-cover object-center"
+          src={urlForImage(photo).width(400).height(300).quality(100).url()}
+          placeholder="blur"
+          blurDataURL={photo.asset.metadata.lqip}
+          width={400}
+          height={300}
+          quality={100}
+          alt={photo.alt}
+        />
       </div>
     </div>
   );
